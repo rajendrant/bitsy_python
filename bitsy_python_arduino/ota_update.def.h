ad564763c36ec885aae113c28bcf0a4a85f3368d
Raj <rajendran.thirupugal@gmail.com>
2018-01-29 20:58:49 +0000

    more fix for ota

--- bitsy_python_arduino/ota_update.def.h
+++ bitsy_python_arduino/ota_update.def.h
@@ -1,31 +1,31 @@
 #ifndef DESKTOP
-#include <EEPROM.h>
 
 bool ota_update(uint8_t *buf, uint8_t len,
                 bool (*sender)(uint8_t *buf, uint8_t len),
                 uint8_t (*receiver)(uint8_t *buf, uint8_t len)) {
   if (len!=12)
     return false;
-  for(uint8_t i=0; i<len; i++)
+  for(uint8_t i=2; i<len; i++)
     if (buf[i] != 11*(i+1))
       return false;
   // Start OTA update.
   int start=millis();
-  uint16_t pos=0;
+  uint16_t pos=0, end=*((uint16_t)buf);
   uint8_t prog[32];
   do {
     if (!sender((uint8_t*)&pos, 2)) return false;
     for(int t=millis(); millis()<t+30;) {
       if(len=receiver(prog, sizeof(prog)) && ((uint16_t*)prog)[0]==pos) {
         for(int i=0; i<len-2; i++)
-          EEPROM.update(pos+i+20, prog[i+2]);
+          EEPROM.update(pos+i, prog[i+2]);
         pos += len-2;
       }
     }
-  } while(millis() < start+10000);
+  } while(pos < end && millis() < start+10000);
   return true;
 }
 
+#ifdef ENABLE_BITSY_USERLIB_SERIAL
 void ota_update_serial() {
   if (Serial.available()) {
     char buf[16];
@@ -51,7 +51,9 @@ void ota_update_serial() {
   }
 
 }
+#endif
 
+#ifdef ENABLE_BITSY_USERLIB_NRF24
 void ota_update_nrf24() {
   uint8_t buf[32], len;
   while(len=nrf24::ota_nrf24_recv(buf, sizeof(buf))) {
@@ -59,10 +61,12 @@ void ota_update_nrf24() {
       nrf24::send_to_callback(buf, len);
   }
 }
+#endif
 
 void ota_update_loop() {
   static int last_ota_check=0;
-  if (millis() > last_ota_check+100) {
+
+  if (millis() > last_ota_check+20) {
     last_ota_check = millis();
 #ifdef ENABLE_BITSY_USERLIB_SERIAL
     ota_update_serial();
