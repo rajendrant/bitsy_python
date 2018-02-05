84510f66a96b3b601d509c7747755ffd1f7d1bbc
Raj <rajendran.thirupugal@gmail.com>
2018-02-05 10:21:56 +0000

    Add more unitttest

--- test/pythontests/test_function1.py
+++ test/pythontests/test_function1.py
@@ -5,6 +5,10 @@ def main():
     print multiply(2,4)
     print pi()
     print divide(22.0, 16)
+    print factorial(5)
+    print factorial(10)
+    print fib(10)
+    print fib(15)
 
 def foo():
     print 1,2,3
@@ -24,3 +28,14 @@ def pi():
 
 def divide(a, b):
     return a/b
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def fib(n):
+    if n<2:
+        return n
+    return fib(n-1)+fib(n-2)
